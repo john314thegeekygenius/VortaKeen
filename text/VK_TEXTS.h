d8ae918b6000e381d7384f2e973b9e42bac8e636
John314 <36487623+john314thegeekygenius@users.noreply.github.com>
2022-06-29 23:31:41 -0400

    Add files via upload

---
+++ text/VK_TEXTS.h
@@ -0,0 +1,87 @@
+
+
+const char *HIGH_SCORE_NAMES[3] = {
+	"Yorpy",
+	"Gargile",
+	"Zzapp!",
+};
+
+const char *VK_STATUE_MESSAGES[16][7] = {
+	{},
+	// Level 2 Yorp statue
+	{
+		"You hear in your    ",
+		"mind:               ",
+		"                    ",
+		"It is too bad that  ",
+		"you cannot read the ",
+		"Standard Galactic   ",
+		"Alphabet, Human.    "
+	},
+	{},
+	{},
+	{},
+	// Level 6 yorp statue
+	{
+		"A message echoes in ",
+		"your head:          ",
+		"                    ",
+		"The Teleporter in   ",
+		"the ice will send   ",
+		"you to the dark side",
+		"of Mars             "
+	},
+	{},
+	{},
+	// Level 9 yorp statue
+	{
+		"A voice buzzes in   ",
+		"your mind:          ",
+	    "                    ",
+	    "There is a hidden   ",
+	    "city. Look in the   ",
+	    "dark area of the    ",
+	    "city to the south   "
+	},
+	// Level 10 yorp statue
+	{
+		"You see these words ",
+		"in your head:       ",
+		"                    ",
+		"You will need a     ",
+		"raygun in the end,  ",
+		"but not to kill the ",
+		"Vorticon...         "
+	},
+	// Level 11 garg statue
+	{
+		"You hear in your    ",
+		"mind:               ",
+		"                    ",
+		" GAAARRRRRGG!       ",
+//		"                    ",
+//		"                    ",
+//		"                    "
+	},	
+	// Level 12 yorp statue
+	{
+		 "A Yorpish whisper   ",
+		 "says:               ",
+		 "                    ",
+		 "Look for dark, hidden",
+		 "bricks. You can see ",
+		 "naught but their    ",
+		 "upper left corner.  "
+	},	
+	{},
+	{},
+	// Level 15 yorp statue
+	{
+		"A Yorpy mind-thought",
+		"bellows:            ",
+		"                    ",
+		"You cannot kill the ",
+		"Vorticon Commander  ",
+		"directly.           "
+	}
+};
